09672d0308a8e1196b05383c6896d8a74fcec359
Hideki Yamamoto <hideki.1516.web@gmail.com>
2023-12-06 22:55:43 +0900

    変更

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home({ allPostsData, response }) {
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Blogだよ！</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
